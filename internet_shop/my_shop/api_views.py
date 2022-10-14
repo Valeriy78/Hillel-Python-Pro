8f78daea88deabf0f33492e3dfa4addc4685ddb9
Valeriy78 <valpasko78@gmail.com>
2022-10-14 23:26:45 +0300

    feat: basic authentication and permissions for API added

--- internet_shop/my_shop/api_views.py
+++ internet_shop/my_shop/api_views.py
@@ -4,6 +4,7 @@ My_shop application API views
 
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
+from internet_shop.permissions import IsSuperuserOrReadOnly
 from internet_shop.serializers import CategorySerializer, ProductSerializer
 from .models import Category, Product
 
@@ -11,19 +12,23 @@ from .models import Category, Product
 class ProductListCreateAPIView(ListCreateAPIView):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
+    permission_classes = [IsSuperuserOrReadOnly]
 
 
 class ProductRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
+    permission_classes = [IsSuperuserOrReadOnly]
 
 
 class CategoryListCreateAPIView(ListCreateAPIView):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+    permission_classes = [IsSuperuserOrReadOnly]
 
 
 class CategoryRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+    permission_classes = [IsSuperuserOrReadOnly]
 
--- internet_shop/purchase/api_views.py
+++ internet_shop/purchase/api_views.py
@@ -4,6 +4,7 @@ Purchase application API views
 
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
+from internet_shop.permissions import IsSuperuserOrReadOnly
 from internet_shop.serializers import OrderSerializer
 from .models import Order
 
@@ -11,8 +12,10 @@ from .models import Order
 class OrderListCreateAPIView(ListCreateAPIView):
     serializer_class = OrderSerializer
     queryset = Order.objects.all()
+    permission_classes = [IsSuperuserOrReadOnly]
 
 
 class OrderRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     serializer_class = OrderSerializer
     queryset = Order.objects.all()
+    permission_classes = [IsSuperuserOrReadOnly]
